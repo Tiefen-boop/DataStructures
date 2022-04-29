ee9a4407014aaa21f5208ef69909c8c4abe261bf
Roi Tiefenbrunn <roitief@post.bgu.ac.il>
2022-04-29 20:30:43 +0300

    Initial commit

---
+++ List.h
@@ -0,0 +1,14 @@
+//
+// Created by Ikak on 29/04/2022.
+//
+
+#ifndef DATASTRUCTURES_LIST_H
+#define DATASTRUCTURES_LIST_H
+
+template < class T>
+class List {
+public:
+    
+};
+
+#endif //DATASTRUCTURES_LIST_H
